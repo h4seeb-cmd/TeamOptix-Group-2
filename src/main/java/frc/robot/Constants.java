7526559c454e2f55f5468da67b2d8e5f9ebd7740
SketchyBoi <whirlwindfury21@gmail.com>
2022-11-15 17:20:34 -0800

    Added random port numbers for soleinoid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,7 @@ package frc.robot;
 public final class Constants {
     public static final class Intake {
         public static final int intake_motor;
-        public static final int piston;
+        public static final int soleinoid_port_one = 1;
+        public static final int soleinoid_port_two = 2;
     }
 }
