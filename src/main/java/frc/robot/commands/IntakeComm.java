1d4f12d43e0364eb263a10e0cb37b63e561e0a84
SketchyBoi <whirlwindfury21@gmail.com>
2022-11-16 16:50:09 -0800

    intakecomm dont yell at me

--- src/main/java/frc/robot/commands/IntakeComm.java
+++ src/main/java/frc/robot/commands/IntakeComm.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 /** An example command that uses an example subsystem. */
 public class IntakeComm extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final IntakeComm intake;
+  private final Intake intake;
 
   /**
    * Creates a new ExampleCommand.
