47eec7d466cd22b704efc8d6dc10da2f2a18fd58
horizon-ab <baileyasay@gmail.com>
2022-11-16 16:38:31 -0800

    woohoo

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,8 +2,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 
 /**
@@ -17,11 +21,18 @@ public class Intake extends SubsystemBase {
     DoubleSolenoid doublePCM1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.soleinoid_one_forward, Constants.Intake.soleinoid_one_backward); // Add ports later
     DoubleSolenoid doublePCM2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.soleinoid_two_forward, Constants.Intake.soleinoid_two_backward); // Add ports later
 
+    CANSparkMax neoMotor = new CANSparkMax(Constants.Intake.intake_motor, CANSparkMax.MotorType.kBrushless);
+
     public Intake() {
 
     }
 
-    public 
+    public void suckBalls() {
+        doublePCM1.set(Value.kForward);
+        doublePCM2.set(Value.kForward);
+
+        neoMotor.set(0);
+    }
 
 
 }
