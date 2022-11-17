861616203d60ae39b912fff5a2382d3db6cef040
horizon-ab <baileyasay@gmail.com>
2022-11-16 16:41:45 -0800

    compression

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,6 +5,7 @@ import frc.robot.Constants;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -20,11 +21,12 @@ public class Intake extends SubsystemBase {
     
     DoubleSolenoid doublePCM1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.soleinoid_one_forward, Constants.Intake.soleinoid_one_backward); // Add ports later
     DoubleSolenoid doublePCM2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.soleinoid_two_forward, Constants.Intake.soleinoid_two_backward); // Add ports later
+    Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
 
     CANSparkMax neoMotor = new CANSparkMax(Constants.Intake.intake_motor, CANSparkMax.MotorType.kBrushless);
 
     public Intake() {
-
+        pcmCompressor.enableDigital();
     }
 
     public void suckBalls() {
